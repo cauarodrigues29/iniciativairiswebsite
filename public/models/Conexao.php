e3fa04e14da45691ede91196b723f12b0ed0e6fe
cauarodrigues29 <107934015+cauarodrigues29@users.noreply.github.com>
2022-07-19 11:51:09 -0300

    Update Conexao.php

--- public/models/Conexao.php
+++ public/models/Conexao.php
@@ -4,7 +4,7 @@
     $senha= '';
     $dbname= 'iris';
 
-   $mysqli = new mysqli($servidor, $usuario, $senha, $dbname);
+   $mysqli = new mysqli($servidor, $usuario, $senha, $dbname, 3307);
     if($mysqli -> error){
         die('Houve um erro: '. $mysqli -> error);
     }
